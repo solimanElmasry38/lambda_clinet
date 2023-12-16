5216f6193e09e1e3c31ae43b1689afe1540792b6
solimanElmasry38 <solimanelmasry38@gmail.com>
2023-12-16 06:50:25 +0200

    s

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.scss";
-import "./Renders/main.ts";
 import { BrowserRouter } from "react-router-dom";
 import {
   ApolloClient,
