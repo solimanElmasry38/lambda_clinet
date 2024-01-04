95b4780e0b2c0bfcc063bb705a6105a2862eeafe
solimanElmasry38 <solimanelmasry38@gmail.com>
2024-01-04 02:44:42 +0200

    s

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,17 +1,30 @@
-import  "./ProductCard.scss"
+import "./ProductCard.scss";
 export const ProductCard = ({ data }) => {
   const { id, name, img, price, count, is_available, desc } = data;
-  console.log("form com p d" + JSON.stringify(data));
-  return (
-    <div className="card1">
-      <span >{id}</span>
-      <span >{name}</span>
-      <span >{img}</span>
-      <span >{price}</span>
-      <span >{count}</span>
-      <span >{is_available}</span>
-      <span >{desc}</span>
 
+  return (
+    <div className="item-container">
+      <div className="main-item">
+        <img
+          src={img}
+          alt=""
+          style={{ width: "200px", height: "200px", objectFit: "cover" }}
+        />
+      </div>
+      <h2 className="item-heading">{name}</h2>
+      <p className="item-description">{desc}</p>
+      <ul className="rating">
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star-half-stroke"></i>
+      </ul>
+      <p className="item-price">
+        <sup>$</sup>
+        {price}
+      </p>
+      <button className="item-cart-btn">Add To Cart</button>
     </div>
   );
 };
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,14 +1,11 @@
-
 import "./home.scss";
 import { Carousel } from "../../components/Carousel/Carousel";
 import { useQuery } from "@apollo/client";
 import { _GetOffers } from "../../gql/query/getOffers";
 import Cookies from "js-cookie";
-import {  useState } from "react";
+import { useState } from "react";
 import { _GetProducts } from "../../gql/query/getPoducts.gql";
 import { ProductCard } from "../../components/ProductCard/ProductCard";
-import { div } from "three/examples/jsm/nodes/Nodes.js";
-
 
 const Home = () => {
   const [images] = useState<string[]>([]);
@@ -21,11 +18,6 @@ const Home = () => {
       },
     },
   });
-  // if (loading) {
-
-  // return <p>loading</p>;
-
-  // }
 
   if (!obj.loading) {
     const arr = obj.data.OFFERS_GET;
@@ -37,51 +29,27 @@ const Home = () => {
 
   const { data, loading } = useQuery(_GetProducts);
 
-  if(loading){
-    return <p>loading</p>
+  if (loading) {
+    return <p>loading</p>;
   }
 
-    return (
-      <>
-        <section className="homeSec">
-          <div className="slider">
-            <Carousel  images={images}/>
-          </div>
-          <div className="homeContainer">
-            {
-              console.log("main"+JSON.stringify(data.PRODUCTS_GET))
-            }
-           {
-            // console.log("main "+);
-            data.PRODUCTS_GET.map(product=>(
-              <div key={product.id}>
-
-                <ProductCard data={product} />
-              </div>
-            ))
-           }
-            {/* <GlassOverLay
-              children={
-                <>
-                  <CourseCard />
-                  <CourseCard />
-                  <CourseCard />
-                  <CourseCard />
-                  <CourseCard />
-                  <CourseCard />
-                  <CourseCard />
-                  <CourseCard />
-                  <CourseCard />
-                  <CourseCard />
-                </>
-              }
-            /> */}
-          </div>
-        </section>
-      </>
-    );
-  }
-
+  return (
+    <>
+      <section className="homeSec">
+        <div className="slider">
+          <Carousel images={images} />
+        </div>
+        <div className="homeContainer">
+          {data.PRODUCTS_GET.map((product) => (
+            <div key={product.id}>
+              <ProductCard data={product} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
 
 // };
 export default Home;
